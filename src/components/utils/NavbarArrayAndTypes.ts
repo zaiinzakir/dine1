0176e5f8ffbbf510245626eb42d048ae79e69669
zaiinzakir <123568404+zaiinzakir@users.noreply.github.com>
2023-10-01 11:04:32 -0700

    Add files via upload

--- src/components/utils/NavbarArrayAndTypes.ts
+++ src/components/utils/NavbarArrayAndTypes.ts
@@ -10,12 +10,48 @@ export const NavbarArray : Array<NavbarItemType> = [
     {
     label: "Female",
     href: "/female",
-    isDropDown: false,
+    isDropDown: true,
+    dropDownData: [
+        {
+            label:"Dresses",
+            href:"/female/dresses"
+        },
+        {
+            label:"Shirts",
+            href:"/female/shirts"
+        },
+        {
+            label:"Pants",
+            href:"/female/pants"
+        },
+        {
+            label:"Accessories",
+            href:"/female/accessories"
+        }
+    ]
     },
     {
         label: "Male",
         href: "/male",
-        isDropDown: false,
+        isDropDown: true,
+        dropDownData: [
+            {
+                label:"Suits",
+                href:"/male/suits"
+            },
+            {
+                label:"Shirts",
+                href:"/male/shirts"
+            },
+            {
+                label:"Pants",
+                href:"/male/pants"
+            },
+            {
+                label:"Accessories",
+                href:"/male/accessories"
+            }
+        ]
     },
     {
         label: "Kids",
--- src/components/views/Navbar/index.tsx
+++ src/components/views/Navbar/index.tsx
@@ -1,22 +1,52 @@
+import {RxHamburgerMenu} from 'react-icons/rx'
+import {RiArrowDropDownLine} from 'react-icons/ri'
+import {BsBasket3} from 'react-icons/bs'
+import {BiSearch} from 'react-icons/bi'
 import React from 'react'
 import Image from "next/image"
 import { NavbarArray, NavbarItemType } from '../../utils/NavbarArrayAndTypes'
 import Link from 'next/link'
+import DropDown from './subComponents/DropDown'
 
 const Navbar = () => {
     return(
-        <div className="py-6 flex">
-            <div className="w-36">
+        <div className="py-6 flex justify-between item-center space-x-12">
+            <div className="w-36 flex-shrink-0">
                 <Image width={500} height={500}  src={"/Logo.webp"} alt="Logo" />
             </div>
-            <div>
-                <ul>
-                {NavbarArray.map((item: NavbarItemType, index:number)=>(
-                    <li>
-                       <Link href={item.href}> {item.label} </Link>
+            <div className='hidden lg:flex justify-between item-center w-full' >
+                <ul className='flex space-x-3 lg:space-x-6 font-medium text-lg'>
+                    {NavbarArray.map((item: NavbarItemType, index:number)=>(
+                    <li className='realtive flex items-center rounded-md px-3 py-1 hover: duration-300 cursor-pointer group'>
+                       <Link  href={item.href}> {item.label} </Link>
+                        {(item.isDropDown ? <RiArrowDropDownLine className=" -rotate-180 group-hover:rotate-0 duration-300"/> : "")}
+                        {item.isDropDown &&
+                            <div className={`invisible group-hover:visible absolute top-16 pr-3 px-2 py-1 rounded-md text-md font-light bg-gray-100 min-w-[6rem]`}>
+                                <DropDown item = {item}/>
+                            </div>
+                        }
                     </li>
-                ))}
+                    ))}
                 </ul>
+            
+                <div className= 'border flex items-center text-gray-600 px-3 rounded-md'>
+                    <BiSearch/>
+                    <input 
+                        type='text' 
+                        className='pr-5 pl-1 py-1 w-80 flex-grow '
+                        placeholder='What are you looking for?'
+                    />
+                </div>
+                <div className='flex-shrink-0 relative w-11 h-11 bg-gray-200 rounded-full flex items-center justify-center'>
+                    <div 
+                        className='absolute w-4 h-4 top-0 right-0 bg-red-400 rounded-full text-xs font-bold flex items-center justify-center'>
+                        2
+                    </div>
+                    <BsBasket3 size={29}/>
+                </div>
+                <div className='flex lg:hidden'>
+                    <RxHamburgerMenu/>
+                </div>
             </div>
         </div>
     )
