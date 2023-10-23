3ad10b1dc5e77b72db97c1b18edc3f52436b97c1
zainzakir <zee.zakir8@gmail.com>
2023-10-23 22:17:13 +0500

    ok

--- src/components/views/Hero/index.tsx
+++ src/components/views/Hero/index.tsx
@@ -7,12 +7,13 @@ const Hero = () => {
     <div className='flex justify-between items-center'>
         {/*left side*/}
         <div>
-            <h1 className=''>An Industrial Take on Streetwear</h1>
+            <h1 className='flex font-bold font-weight-700'>An Industrial Take on Streetwear</h1>
         </div>
 
         {/*right side*/}
-        <div className="">
-            <Image src={HeroMain} alt="HeroMain" />
+        
+        <div className="bg-orange-100 w-72 h-72 rounded-full">
+            <Image src={HeroMain} alt="HeroMain" size={42}/>
         </div>
     </div>
   )
